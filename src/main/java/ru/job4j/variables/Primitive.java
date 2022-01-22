4d3e8e9b8b4b076b5295dde1ecea82e150300cbe
Daimon-J <daimon.java.best@gmail.com>
2022-01-22 13:57:03 -0800

    1. Определите тип данных для переменных. [#172634 #188930]

---
+++ src/main/java/ru/job4j/variables/Primitive.java
@@ -0,0 +1,22 @@
+package ru.job4j.variables;
+
+public class Primitive {
+    public static void main(String[] args) {
+        byte age = 21;
+        char male = 'M';
+        char female = 'W';
+        float height = 1.80f;
+        short bricks = 1220;
+        long bacteria = 231200;
+        boolean permission = true;
+        System.out.println(
+                age + " / " +
+                male + " / " +
+                female + " / " +
+                height + " / " +
+                bricks + " / " +
+                bacteria + " / " +
+                permission + " / "
+        );
+    }
+}
