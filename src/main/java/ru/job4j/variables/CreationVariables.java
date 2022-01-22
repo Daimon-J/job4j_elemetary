8cb34338d154a613a060fb6e0c89ca970be9d3d5
Daimon-J <daimon.java.best@gmail.com>
2022-01-22 13:45:06 -0800

    0. Что такое переменная. [#3610 #188705]

---
+++ src/main/java/ru/job4j/variables/CreationVariables.java
@@ -0,0 +1,10 @@
+package ru.job4j.variables;
+
+public class CreationVariables {
+    public static void main(String[] args) {
+        int age = 18;
+        float length = 78.0332f;
+        boolean baby = true;
+        System.out.println(age + " / " + length + " / " + baby + " / ");
+    }
+}
