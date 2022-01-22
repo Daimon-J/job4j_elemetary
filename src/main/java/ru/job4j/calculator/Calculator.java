6814dfbe706b5bc7af5b609c6bb1873335990167
Daimon-J <daimon.java.best@gmail.com>
2022-01-22 14:11:24 -0800

    3. Арифметические операции. [#185 #188950]

---
+++ src/main/java/ru/job4j/calculator/Calculator.java
@@ -0,0 +1,20 @@
+package ru.job4j.calculator;
+
+public class Calculator {
+    public static void main(String[] args) {
+
+        int six = 6;
+        int four = 4;
+        int five = 5;
+
+        int sixDivTwo = six / 2;
+        int fiveMinusTwo = five - 2;
+        int fourTimesTwo = four * 2;
+
+        System.out.println(
+                "Result: " + sixDivTwo + "  " +
+                "Result: " + fiveMinusTwo + "  " +
+                "Result: " + fourTimesTwo
+        );
+    }
+}
