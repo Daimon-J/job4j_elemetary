f7fd6310457b3c799c913af7c40ec5082d20d14a
Daimon-J <daimon.java.best@gmail.com>
2022-02-03 01:28:16 -0800

    6. Идеальный вес [#41584 #189026]

---
+++ src/main/java/ru/job4j/calculator/Fit.java
@@ -0,0 +1,19 @@
+package ru.job4j.calculator;
+
+public class Fit {
+
+    public static double manWeight(short height) {
+        return (height - 100) * 1.15;
+    }
+
+    public static double womanWeight(short height) {
+        return (height - 110) * 1.15;
+    }
+
+    public static void main(String[] args) {
+        short manHeight = 180, womanHeight = 150;
+        double m = manWeight(manHeight);
+        double w = womanWeight(womanHeight);
+        System.out.println(w + " / " + m);
+    }
+}
